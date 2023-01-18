8d8e3f26ff4b6243662d007d0289a171b833ec20
Jason Zhang <zhangjunjie@shinetechsoftware.com>
2023-01-18 18:29:41 +0800

    Add files via upload

---
+++ DWS/Services.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+
+namespace Utility.DWS
+{
+    public class Services
+    {
+        public DwsConfig Request { get; set; }
+        public Assembly CurrentAssembly { get; protected set; }
+        public Type CurrentClass { get; protected set; }
+        public object CurrentObj { get; protected set; }
+
+        public Services(DwsConfig request)
+        {
+            Request = request;
+
+            using (var stream = Request.GetWSDL())
+            {
+                CurrentAssembly = Helper.GetAssembly(Request.Namespace, stream);
+            }
+                
+            CurrentClass = CreateClass(Request.ClassName);
+            CurrentObj = Activator.CreateInstance(CurrentClass);
+
+            foreach (var rev in Request.Revises)
+            {
+                var pTimeout = CurrentClass.GetProperty(rev.Key);
+                if (pTimeout != null) pTimeout.SetValue(CurrentObj, rev.Value, null);
+            }
+        }
+
+        public object Invoke(string methodName, params object[] args)
+        {
+            var m = CurrentClass.GetMethod(methodName);
+
+            return m.Invoke(CurrentObj, args);
+        }
+
+        public Type CreateClass(string className)
+        {
+            return CurrentAssembly.GetType(string.Concat(Request.Namespace, ".", className), true, true);
+        }
+
+        public object CreateObject(string className)
+        {
+            return Activator.CreateInstance(CreateClass(className));
+        }
+    }
+}
---
+++ Email/SendingResult.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Mail;
+
+namespace Utility.Email
+{
+    #region Sending Result
+    public class SendingResult
+    {
+        public bool IsSent
+        {
+            get
+            {
+                return First.Successful || (TryAgain != null && TryAgain.Successful);
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get
+            {
+                var msg = "";
+
+                if (!IsSent)
+                {
+                    msg = TryAgain != null ? TryAgain.Error.Message : First.Error.Message;
+                }
+
+                return msg; 
+            }
+        }
+
+        public SingleResult First { get; set; }
+        public SingleResult TryAgain { get; set; }
+    }
+    #endregion
+
+    #region Single Result
+    public class SingleResult
+    {
+        public bool Successful { get; set; }
+        public DateTime BeginTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public SmtpException Error { get; set; }
+
+        public SingleResult BeginSend()
+        {
+            BeginTime = DateTime.Now;
+
+            return this;
+        }
+
+        public void EndSend(SmtpException ex = null)
+        {
+            EndTime = DateTime.Now;
+            Error = ex;
+            Successful = ex == null;
+        }
+    }
+    #endregion 
+}
